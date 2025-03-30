ad0d1640497fd583c8ed00a3e49b9b6111c39bf6
Lucifugus0 <130909171+Lucifugus0@users.noreply.github.com>
2025-03-30 13:22:10 +0700

    push

--- src/app/page.js
+++ src/app/page.js
@@ -55,6 +55,6 @@ export default function Home() {
       <section id="portfolio" className="min-h-screen flex items-center justify-center p-6">
         <h1 className="text-4xl font-bold">My Portfolio</h1>
       </section>
-    </div>
+      </div>
   );
 }
